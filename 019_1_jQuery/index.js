4462a8a408748976e6473c3750cfe9d8161bd4e7
Dmitriy Kudasov <dev.kudasov@gmail.com>
2025-12-07 17:37:48 +0300

    Add input field and enhance jQuery functionality: include a text input in HTML, add click and keypress event handlers for header color changes, and modify mouseover effects for improved interactivity.

--- 019_1_jQuery/index.js
+++ 019_1_jQuery/index.js
@@ -2,5 +2,20 @@ $('h1').addClass('big-title margin-50');
 console.log($('h1').hasClass('margin-50'));
 
 $('button').text("Don't click me");
+$('button').click(function () {
+  $('h1').css('color', 'purple');
+});
 
 $('a').attr('href', 'https://www.bing.com');
+
+$(document).keypress(function (e) {
+  $('h1').text(e.key);
+});
+
+$('h1').on('mouseover', function () {
+  $('h1').css('color', 'purple');
+});
+
+$('h1').on('mouseleave', function () {
+  $('h1').css('color', 'red');
+});
