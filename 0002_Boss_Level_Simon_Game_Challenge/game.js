11f1eee9f78768be9d5323db4ec856a35dc3a3b5
Dmitriy Kudasov <dev.kudasov@gmail.com>
2025-12-07 21:46:17 +0300

    Refactor Simon Game logic: Introduce level tracking, sound playback, and button animation for user interactions in game.js.

--- 0002_Boss_Level_Simon_Game_Challenge/game.js
+++ 0002_Boss_Level_Simon_Game_Challenge/game.js
@@ -2,17 +2,42 @@ const COLORS = ['red', 'blue', 'green', 'yellow'];
 const gamePattern = [];
 const userPattern = [];
 
-function newSequence() {
+let level = 0;
+
+function nextSequence() {
+  upgradeLevel();
   let randomColor = Math.floor(Math.random() * 4);
   const color = COLORS[randomColor];
   gamePattern.push(color);
   $(`#${color}`).fadeIn(100).fadeOut(100).fadeIn(100);
-  const sound = new Audio(`sounds/${color}.mp3`);
-  sound.play();
+  playSound(color);
 }
 
 $('[type="button"]').click(function () {
   const userChosenColour = $(this).attr('id');
+
   userPattern.push(userChosenColour);
-  console.log(userPattern);
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
 });
+
+function playSound(color) {
+  const sound = new Audio(`sounds/${color}.mp3`);
+  sound.play();
+}
+
+function animatePress(color) {
+  $(`#${color}`).addClass('pressed');
+  setTimeout(() => {
+    $(`#${color}`).removeClass('pressed');
+  }, 100);
+}
+
+function upgradeLevel() {
+  level++;
+  $('#level-title').text(`Level ${level}`);
+}
+
+nextSequence();
+nextSequence();
+nextSequence();
