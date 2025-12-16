713d18f8a80ef9075bfe872ad7a2ba80d1b6ea76
Dmitriy Kudasov <dev.kudasov@gmail.com>
2025-12-16 16:25:03 +0300

    Refactor index1.js to use path.join for file paths and update package.json to include nodemon for development. Added new start and dev scripts for multiple index files.

--- 24_3_Middleware/index1.js
+++ 24_3_Middleware/index1.js
@@ -1,13 +1,13 @@
-import express from "express";
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-const __dirname = dirname(fileURLToPath(import.meta.url));
+import express from 'express';
+import path from 'path';
+const indexPath = path.join(import.meta.dirname, 'public', 'index.html');
 
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/public/index.html");
+app.get('/', (req, res) => {
+  console.log(indexPath);
+  res.sendFile(indexPath);
 });
 
 app.listen(port, () => {
