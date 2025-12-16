cfe67bdf1eeab9e3629f9922effe9399a8490f01
Dmitriy Kudasov <dev.kudasov@gmail.com>
2025-12-16 14:27:42 +0300

    Add routes for home, about, and contact pages in the Express server; include nodemon for development in package.json

--- 024_2_HTTP_Requests/index.js
+++ 024_2_HTTP_Requests/index.js
@@ -3,6 +3,18 @@ const app = express();
 
 const PORT = 3000;
 
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World</h1>');
+});
+
+app.get('/about', (req, res) => {
+  res.send('<h1>About</h1><p>This is the about page</p>');
+});
+
+app.get('/contact', (req, res) => {
+  res.send('<h1>Contact</h1><p>This is the contact page</p>');
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
