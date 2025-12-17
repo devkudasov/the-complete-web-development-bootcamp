f10a28a3a6da0169897281fbf8cf4fd37089cb91
Dmitriy Kudasov <dev.kudasov@gmail.com>
2025-12-17 12:23:49 +0300

    Implement logging middleware and update root route response in index2.js

--- 24_3_Middleware/index2.js
+++ 24_3_Middleware/index2.js
@@ -1,12 +1,21 @@
-import express from "express";
+import express from 'express';
+import morgan from 'morgan';
 
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello");
+app.use(logger);
+
+app.get('/', (req, res) => {
+  console.log('Hello');
+  res.send('Hello');
 });
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
+
+function logger(req, res, next) {
+  console.log(`${req.method} | ${req.url} | ${new Date().toISOString()}`);
+  next();
+}
