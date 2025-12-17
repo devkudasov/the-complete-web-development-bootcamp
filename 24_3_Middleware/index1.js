fb08dd11c061c078ca9dff74edb7191fc795e029
Dmitriy Kudasov <dev.kudasov@gmail.com>
2025-12-17 11:39:38 +0300

    Add body-parser middleware to handle form submissions and implement a POST route for '/submit' in index1.js.

--- 24_3_Middleware/index1.js
+++ 24_3_Middleware/index1.js
@@ -1,13 +1,20 @@
 import express from 'express';
 import path from 'path';
+import bodyParser from 'body-parser';
+
 const indexPath = path.join(import.meta.dirname, 'public', 'index.html');
 
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
-  console.log(indexPath);
   res.sendFile(indexPath);
+});
+
+app.post('/submit', (req, res) => {
+  console.log(req.body);
 });
 
 app.listen(port, () => {
