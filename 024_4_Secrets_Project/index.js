76f801ac7b9f77fe4c0c030165d456c80fe56402
Dmitriy Kudasov <dev.kudasov@gmail.com>
2025-12-17 12:42:10 +0300

    Refactor Secrets Project: Replace solution.js with index.js for Express server setup, implement password check functionality, and add nodemon for development. Update package.json and package-lock.json to include new dependencies.

--- 024_4_Secrets_Project/index.js
+++ 024_4_Secrets_Project/index.js
@@ -1,3 +1,29 @@
-//To see how the final website should work, run "node solution.js".
-//Make sure you have installed all the dependencies with "npm i".
-//The password is ILoveProgramming
+import express from 'express';
+import bodyParser from 'body-parser';
+import path from 'path';
+
+const app = express();
+const port = 3000;
+
+const publicPath = path.join(import.meta.dirname, 'public');
+const indexPath = path.join(publicPath, 'index.html');
+const secretPath = path.join(publicPath, 'secret.html');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.sendFile(indexPath);
+});
+
+app.post('/check', (req, res) => {
+  const password = req.body.password;
+  if (password === 'ILoveProgramming') {
+    res.sendFile(secretPath);
+  } else {
+    res.sendFile(indexPath);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
