00d2bab4302bcdea8ccd9b7f64605aa6103bc7ab
Kudasov Dmitriy <dev.kudasov@gmail.com>
2025-12-17 13:59:30 +0300

    25.1 Solution (#53)
    * Init project
    
    * Add Express server setup with EJS templating and nodemon for development

---
+++ 025_1_EJS/index.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import path from 'path';
+import ejs from 'ejs';
+
+const app = express();
+const port = 3000;
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(import.meta.dirname, 'views'));
+
+app.get('/', (req, res) => {
+  const day = new Date().getDay();
+  const isWeekday = day >= 0 && day <= 4;
+  res.render('index', { isWeekday: isWeekday });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
