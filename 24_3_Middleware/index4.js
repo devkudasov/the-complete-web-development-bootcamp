21d61e8058c9d4f025992b29d7430160c884cd13
Dmitriy Kudasov <dev.kudasov@gmail.com>
2025-12-17 12:30:17 +0300

    Add morgan and body-parser middleware; implement GET and POST routes for serving index.html and handling form submissions in index4.js.

--- 24_3_Middleware/index4.js
+++ 24_3_Middleware/index4.js
@@ -1,7 +1,25 @@
-import express from "express";
+import express from 'express';
+import morgan from 'morgan';
+import bodyParser from 'body-parser';
+import path from 'path';
+
+const indexPath = path.join(import.meta.dirname, 'public', 'index.html');
 
 const app = express();
 const port = 3000;
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.sendFile(indexPath);
+});
+
+app.post('/submit', (req, res) => {
+  res.send(
+    `<h1>Your band name is:</h1><h2>${req.body.street}${req.body.pet}</h2>`
+  );
+});
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
