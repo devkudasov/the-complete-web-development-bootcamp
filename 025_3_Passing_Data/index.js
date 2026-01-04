e186163735914ac34421f2d78eb6f819dd4dc23e
Dmitriy Kudasov <dev.kudasov@gmail.com>
2026-01-04 15:40:26 +0300

    index.js BL implementation

--- 025_3_Passing_Data/index.js
+++ 025_3_Passing_Data/index.js
@@ -1,14 +1,27 @@
-import express from "express";
-import bodyParser from "body-parser";
+import express from 'express';
+import bodyParser from 'body-parser';
 
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
 
-app.get("/", (req, res) => {});
+function getNumberOfLetters(fName, lName) {
+  return (fName?.length ?? 0) + (lName?.length ?? 0);
+}
 
-app.post("/submit", (req, res) => {});
+app.get('/', (req, res) => {
+  res.render('index.ejs', {
+    numberOfLetters: getNumberOfLetters(req.body.fName, req.body.lName),
+  });
+});
+
+app.post('/submit', (req, res) => {
+  res.render('index.ejs', {
+    numberOfLetters: getNumberOfLetters(req.body.fName, req.body.lName),
+  });
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
