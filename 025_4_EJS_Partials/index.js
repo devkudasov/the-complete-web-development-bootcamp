d0156b36258c84c3b0d81b7354a711fd83ecdc6b
Kudasov Dmitriy <dev.kudasov@gmail.com>
2026-01-04 15:59:36 +0300

    025.4 Solution (#57)
    * Init
    
    * Solution

---
+++ 025_4_EJS_Partials/index.js
@@ -0,0 +1,26 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+/* Write your code here:
+Step 4: Add the partials to the about and contact pages to show the header and footer on those pages. */
+
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('index.ejs');
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.ejs');
+});
+
+app.get('/contact', (req, res) => {
+  res.render('contact.ejs');
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
